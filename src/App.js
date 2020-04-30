500633cb34edfc125ceff76e95e8e2de59d5263e
Kristina Alekyan <kristinaalekyan1@gmail.com>
2020-04-30 16:13:33 +0400

    first commit

---
+++ src/App.js
@@ -0,0 +1,83 @@
+import React, { useState } from "react";
+import './App.css';
+
+
+
+function Todo({ todo, index, completeTodo }) {
+  return (
+    <div
+      className="todo"
+      style={{ textDecoration: todo.isCompleted ? "line-through" : "" }}
+    >
+      {todo.text}
+
+      <div>
+        <button onClick={() => completeTodo(index)}>Complete</button>
+      </div>
+    </div>
+  );
+}
+
+function TodoForm({ addTodo }) {
+  const [value, setValue] = useState("");
+
+  const handleSubmit = e => {
+    e.preventDefault();
+    if (!value) return;
+    addTodo(value);
+    setValue("");
+  };
+
+  return (
+    <form onSubmit={handleSubmit}>
+      <input
+        type="text"
+        className="input"
+        value={value}
+        onChange={e => setValue(e.target.value)}
+      />
+    </form>
+  );
+}
+
+function App() {
+  const [todos, setTodos] = useState([
+    {
+      text: "Waik up",
+      isCompleted: false
+    },
+    
+
+    
+  ]);
+
+  const addTodo = text => {
+    const newTodos = [...todos, { text }];
+    setTodos(newTodos);
+  };
+  const completeTodo = index => {
+    const newTodos = [...todos];
+    newTodos[index].isCompleted = true;
+    setTodos(newTodos);
+  };
+
+
+
+  return (
+    <div className="app">
+      <p><b>To Do List</b></p>
+      <div className="todo-list">
+        {todos.map((todo, index) => (
+          <Todo
+            key={index}
+            index={index}
+            todo={todo}
+            completeTodo={completeTodo}
+          />
+        ))}
+        <TodoForm addTodo={addTodo} completeTodo={completeTodo} />
+      </div>
+    </div>
+  );
+}
+  export default App;
